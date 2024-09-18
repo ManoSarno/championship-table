b648ae88935a2175b37ffc8803b11deff1e68763
ManoSarno <manusarno97@gmail.com>
2024-09-18 00:56:08 -0300

    Function that renders list items for each game of current round and changes displayed round number.

--- src/rounds.ts
+++ src/rounds.ts
@@ -4,20 +4,54 @@ import { fetchSoccerResults } from "./fetchData";
 export async function rounds(): Promise<void> {
   // Array of SoccerRound objects fetched by API
   const gamesData: SoccerRound[] | undefined = await fetchSoccerResults();
-  console.log(gamesData);
 
   // Current round
   let currentRoundIndex: number = 0;
 
+  // Elements to modify when round changes
+  const gamesList: HTMLElement | null = document.getElementById("gamesList");
+  const currentRound: HTMLElement | null = document.getElementById("currentRound");
+  
   // Button elements that control navigation between rounds
   const prevRoundBtn: HTMLButtonElement | null = document.getElementById("prevRound") as HTMLButtonElement;
   const nextRoundBtn: HTMLButtonElement | null = document.getElementById("nextRound") as HTMLButtonElement;
+
+  // Function that renders elements with information for the respective round
+  function renderRound(roundIndex: number): void {
+    if (!gamesList || !currentRound || !prevRoundBtn || !nextRoundBtn) return;
+
+    // Select current round object
+    const roundData: SoccerRound = gamesData![roundIndex];
+    
+    // Modify displayed round number
+    currentRound.textContent = `RODADA ${roundData.round}`;
+
+    // Reset elements inside games list
+    gamesList.innerHTML = "";
+
+    // Iterate through each game in roundData, create a li element
+    // and append it to the games list element
+    roundData.games.forEach((game: Game) => {
+      const listItem: HTMLLIElement = document.createElement("li");
+      listItem.classList.add("game-item");
+      
+      listItem.innerHTML = `
+        
+      `;
+
+      gamesList.appendChild(listItem);
+    });
+
+    // Disable buttons when reaching min/max of rounds
+    prevRoundBtn.disabled = roundIndex === 0;
+    nextRoundBtn.disabled = roundIndex === gamesData!.length - 1;
+  }
 
   // Event listener for prev button, renders prev round unless is the first
   prevRoundBtn?.addEventListener("click", () => {
     if (currentRoundIndex > 0) {
       currentRoundIndex--;
-      console.log(currentRoundIndex);
+      renderRound(currentRoundIndex);
     }
   });
 
@@ -25,7 +59,10 @@ export async function rounds(): Promise<void> {
   nextRoundBtn?.addEventListener("click", () => {
     if (currentRoundIndex < gamesData!.length - 1) {
       currentRoundIndex++;
-      console.log(currentRoundIndex);
+      renderRound(currentRoundIndex);
     }
   });
+
+  // Initial render
+  renderRound(currentRoundIndex);
 }
